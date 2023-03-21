b4f935b3b58bdada44d6fd553603a0fc1e235acd
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2023-03-21 22:50:51 +0900

    add sidebar

--- src/components/Main/MainVideo/MainVideo.js
+++ src/components/Main/MainVideo/MainVideo.js
@@ -18,7 +18,7 @@ const MainVideo = (props) => {
               src="https://www.youtube.com/embed/dQw4w9WgXcQ"
               title="YouTube video player"
               frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             />
           </div>
